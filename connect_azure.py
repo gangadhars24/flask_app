5080c26fc12a8d9837c4b77bd203a887107b3888
Gangadhar S <gangadhar.shanka@tigeranalytics.com>
2022-08-18 14:45:09 +0530

    ENH: Developed code to connect to azure blob storage

---
+++ connect_azure.py
@@ -0,0 +1,23 @@
+from azure.storage.blob import BlobServiceClient
+from azure.storage.blob import ContainerClient
+import os
+
+
+class AzureBlob:
+    def auth_connection_string(self):
+        # [START auth_from_connection_string]
+        connect_str = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
+
+        # Create the BlobServiceClient object which will be used to
+        # create a container client
+        BlobServiceClient.from_connection_string(connect_str)
+
+        container_client = ContainerClient.from_connection_string(
+            connect_str, container_name="gangadharcontainer"
+        )
+        return container_client
+
+
+if __name__ == "__main__":
+    ab = AzureBlob()
+    ab.auth_connection_string()
