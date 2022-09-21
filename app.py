d700f94768b6729d87206c929aa73830a6e413f0
Gangadhar S <gangadhar.shanka@tigeranalytics.com>
2022-09-21 17:51:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -117,4 +117,4 @@ def download_model():
 model = download_model()
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=500)
+    app.run(debug=True, host="0.0.0.0", port=5000)
